38237dac70cb8cf08ee13b0b1adc818125849a6a
Bradley Remedios <bremedios@gmail.com>
2024-12-01 13:57:04 -0800

    bpl::sys::string::tolower - Fixed namespace method is in

--- libbpl_sys/include/bpl/sys/String.h
+++ libbpl_sys/include/bpl/sys/String.h
@@ -1,21 +1,19 @@
 //
 // Created by Bradley Remedios on 11/21/24.
 //
-#ifndef UTILS_STRING_H
-#define UTILS_STRING_H
+#ifndef BPL_SYS_STRING_H
+#define BPL_SYS_STRING_H
 
 #include <string>
 #include <algorithm>
 
-namespace utils {
-    namespace string {
-        std::string inline tolower(std::string s)
-        {
-            std::ranges::transform(s.begin(), s.end(), s.begin(),
-                           [](unsigned char c){ return std::tolower(c); }
-                          );
-            return s;
-        }
-    };
-}
-#endif //UTILS_STRING_H
+namespace bpl::sys::string {
+    std::string inline tolower(std::string s)
+    {
+        std::ranges::transform(s.begin(), s.end(), s.begin(),
+                       [](unsigned char c){ return std::tolower(c); }
+                      );
+        return s;
+    } // tolower
+}; // bpl::sys
+#endif //BPL_SYS_STRING_H
