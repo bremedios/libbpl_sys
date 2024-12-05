1da60af332edb43a43a8e06920ba12615ccb6bb9
Bradley Remedios <bremedios@gmail.com>
2024-12-05 10:35:28 -0800

    bpl::sys::Debug.h - Added ERROR: and DEBUG: tags to messages

--- libbpl_sys/src/Debug.h
+++ libbpl_sys/src/Debug.h
@@ -9,10 +9,10 @@
 
 //#define __ENABLE_BPL_SYS_DEBUG    1
 
-#define ERROR_MSG(msg)  { std::cerr << __PRETTY_FUNCTION__ << ":" << msg << std::endl; }
+#define ERROR_MSG(msg)  { std::cerr << "ERROR: " << __PRETTY_FUNCTION__ << ":" << msg << std::endl; }
 
 #if defined(__ENABLE_BPL_SYS_DEBUG)
-    #define DEBUG_MSG(msg)  { std::cout << __PRETTY_FUNCTION__ << ":" << msg << std::endl; }
+    #define DEBUG_MSG(msg)  { std::cout << "DEBUG: " << __PRETTY_FUNCTION__ << ":" << msg << std::endl; }
 #else
     #define DEBUG_MSG(cmd)
 #endif // __ENABLE_BPL_SYS_DEBUG
